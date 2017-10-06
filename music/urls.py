ace34c0db605abbe59d77073b8b72b04795e37c3
Schifty87 <ryan.j.schiferl@gmail.com>
2017-10-06 17:24:09 -0500

    Continuing django

--- music/urls.py
+++ music/urls.py
@@ -10,4 +10,13 @@ urlpatterns = [
     url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
     #/music/album/add/ -- no primary key with album yet in this instance
     url(r'album/add/$', views.AlbumCreate.as_view(), name='album-add'),
+
+
+    url(r'album/(?P<pk>[0-9]+)/$', views.AlbumUpdate.as_view(), name='album-update'),
+
+    url(r'album/(?P<pk>[0-9]+)/delete/$', views.AlbumDelete.as_view(), name='album-delete'),
+
+
+
+
 ]
--- music/views.py
+++ music/views.py
@@ -1,6 +1,14 @@
 from django.views import generic
 from .models import Album
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.core.urlresolvers import reverse_lazy
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.views.generic import View
+from .forms import AlbumForm, SongForm
+
+AUDIO_FILE_TYPES = ['wav', 'mp3', 'ogg']
+IMAGE_FILE_TYPES = ['png', 'jpg', 'jpeg']
 
 class IndexView(generic.ListView):
     #when you get a list of albums, plug them into index template
@@ -20,3 +28,19 @@ class AlbumCreate(CreateView):
     model = Album
     #what attributes do you want user to input?
     fields = ['artist','album_title', 'genre', 'album_logo']
+
+#inheret from UpdateView
+class AlbumUpdate(UpdateView):
+    model = Album
+    fields = ['artist','album_title', 'genre', 'album_logo']
+
+class AlbumDelete(DeleteView):
+    #deleting Album object
+    model = Album
+    #redirect to homepage
+    success_url = reverse_lazy('music:index')
+
+
+
+
+
